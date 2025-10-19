2fabcc27e5a96a73bdcbb5e1e6a078a9cd6df6b1
davidojo1144 <davco1144@gmail.com>
2025-10-19 13:58:00 +0100

    deployment of client and server side

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://streamify-chat-video-call-app.vercel.app/',
+  baseURL: 'https://streamify-chat-video-call-app.vercel.app/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
